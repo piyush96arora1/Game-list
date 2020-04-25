8589ddbbf93f2ba0d2f22b743435253a5522bb72
Piyush <Piyush@CORP.AMAZONWORKSPACES.COM>
2020-04-25 18:00:19 +0530

    Updates

---
+++ precache-manifest.66d7d60905e4c176077145c74126f1be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e7a32e84e045ae386e7",
+    "url": "/Game-list/static/css/main.bae3fc49.chunk.css"
+  },
+  {
+    "revision": "6e7a32e84e045ae386e7",
+    "url": "/Game-list/static/js/main.17f76da3.chunk.js"
+  },
+  {
+    "revision": "3c2d99853af4ee876036",
+    "url": "/Game-list/static/js/runtime~main.765549db.js"
+  },
+  {
+    "revision": "0f49f4e3f880c7f97c0d",
+    "url": "/Game-list/static/js/2.19d90555.chunk.js"
+  },
+  {
+    "revision": "eba3b045ff8101af0bdb008235ae2b9b",
+    "url": "/Game-list/index.html"
+  }
+];
